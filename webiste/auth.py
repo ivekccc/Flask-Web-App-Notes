17c02ed7001ec200690b5d9442ca0a2b32c113c0
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-03-21 13:36:23 +0100

    Funkcionalnost kreiranja novog naloga dodata

--- webiste/auth.py
+++ webiste/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint,render_template,request,flash
+from flask import Blueprint,render_template,request,flash,redirect,url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 auth=Blueprint('auth',__name__)
 
@@ -14,18 +17,22 @@ def logout():
 def sign_up():
     if request.method=='POST':
         email=request.form.get('email')
-        firsName=request.form.get('firstName')
+        first_name=request.form.get('firstName')
         password1=request.form.get('password1')
         password2=request.form.get('password2')
 
         if len(email)<4:
             flash('Email must be longer than 3 characters.',category="error")
-        elif len(firsName)<2:
+        elif len(first_name)<2:
             flash('First name must bre longer than 1 character.',category="error")
         elif password1!=password2:
             flash('Passwords don\'t match',category="error")
         elif len(password1)<7:
             flash('Password must be at least 7 characters.',category="error")
         else:
+            new_user=User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!',category="success")
+            return redirect(url_for('views.home'))
     return render_template("sign_up.html")
