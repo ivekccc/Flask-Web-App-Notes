66c7a6eab16f24420b596d51a5568131d438cb4e
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-03-21 23:06:54 +0100

    Ispravljena greska kod kreiranja naloga

--- webiste/auth.py
+++ webiste/auth.py
@@ -30,29 +30,32 @@ def logout():
     return redirect(url_for('auth.login'))
 
 @auth.route("/sign-up",methods=['GET','POST'])
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
-    if request.method=='POST':
-        email=request.form.get('email')
-        first_name=request.form.get('firstName')
-        password1=request.form.get('password1')
-        password2=request.form.get('password2')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-        user=User.query.filter_by(email=email).first()
+        user = User.query.filter_by(email=email).first()
         if user:
-            flash('This email already in use.',category="error")
-        elif len(email)<4:
-            flash('Email must be longer than 3 characters.',category="error")
-        elif len(first_name)<2:
-            flash('First name must bre longer than 1 character.',category="error")
-        elif password1!=password2:
-            flash('Passwords don\'t match',category="error")
-        elif len(password1)<7:
-            flash('Password must be at least 7 characters.',category="error")
+            flash('This email is already in use.', category="error")
+            return redirect(url_for('auth.sign_up'))  # Redirect back to sign-up page
+        elif len(email) < 4:
+            flash('Email must be longer than 3 characters.', category="error")
+        elif len(first_name) < 2:
+            flash('First name must be longer than 1 character.', category="error")
+        elif password1 != password2:
+            flash('Passwords don\'t match', category="error")
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category="error")
         else:
-            new_user=User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user,remember=True)
-            flash('Account created!',category="success")
+            login_user(new_user, remember=True)  # Log in the new user, not the queried user
+            flash('Account created!', category="success")
             return redirect(url_for('views.home'))
-    return render_template("sign_up.html",user=current_user)
+
+    return render_template("sign_up.html", user=current_user)
