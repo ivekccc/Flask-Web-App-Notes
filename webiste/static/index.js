ea8695a76a389bd9844ae459121063e5d9f0098a
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-03-22 10:27:51 +0100

    Funkcija update notes

--- webiste/static/index.js
+++ webiste/static/index.js
@@ -6,3 +6,33 @@ function deleteNote(noteId) {
       window.location.href = "/";
     });
   }
+
+  $(document).ready(function() {
+    $('.edit-btn').click(function() {
+      var noteId = $(this).data('noteid');
+      var noteContent = $(this).siblings('span').text();
+      $('#editModal').data('noteid', noteId);
+      $('#editNoteContent').val(noteContent);
+    });
+
+    $('#saveChangesBtn').click(function() {
+      console.log("jes")
+      var noteId = $('#editModal').data('noteid');
+      var newContent = $('#editNoteContent').val();
+      $.ajax({
+        type: 'POST',
+        url: '/edit-note',
+        contentType: 'application/json',
+        data: JSON.stringify({ noteId: noteId, newContent: newContent }),
+        success: function(response) {
+          alert(response.message);
+          $('#editModal').modal('hide');
+          location.reload();
+        },
+        error: function(error) {
+          alert(error.responseJSON.error);
+        }
+      });
+    });
+  });
+
--- webiste/views.py
+++ webiste/views.py
@@ -6,32 +6,42 @@ import json
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST':
-        note = request.form.get('note')#Gets the note from the HTML
-
+        note = request.form.get('note')
         if len(note) < 1:
             flash('Note is too short!', category='error')
         else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note
-            db.session.add(new_note) #adding the note to the database
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category='success')
 
     return render_template("home.html", user=current_user)
 
+@views.route('/edit-note', methods=['POST'])
+@login_required
+def edit_note():
+    note_data = json.loads(request.data)
+    note_id = note_data['noteId']
+    new_content = note_data['newContent']
+    note = Note.query.get(note_id)
+    if note and note.user_id == current_user.id:
+        note.data = new_content
+        db.session.commit()
+        return jsonify({'message': 'Note updated successfully'})
+    return jsonify({'error': 'Unauthorized or Note not found'}), 403
 
 @views.route('/delete-note', methods=['POST'])
+@login_required
 def delete_note():
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
+    note_data = json.loads(request.data)
+    note_id = note_data['noteId']
+    note = Note.query.get(note_id)
+    if note and note.user_id == current_user.id:
+        db.session.delete(note)
+        db.session.commit()
+        return jsonify({'message': 'Note deleted successfully'})
+    return jsonify({'error': 'Unauthorized or Note not found'}), 403
